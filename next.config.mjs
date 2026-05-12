d4ceaefbaf3d11c658e0217c9e72062f49e1b0bd
Abdul Hannan <114971771+abdul-hannan-SE@users.noreply.github.com>
2026-05-12 14:45:04 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: false, // Disable React Strict Mode to prevent double renders in development
+};
+
+export default nextConfig;
